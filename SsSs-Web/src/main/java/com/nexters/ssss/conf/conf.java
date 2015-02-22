9217cd783f6eec1ad2f7586990533c6203780a53
wow5468 <wow5468@naver.com>
2015-02-22 21:55:10 +0900

    Modify location

--- SsSs-Web/src/main/java/com/nexters/ssss/conf/conf.java
+++ SsSs-Web/src/main/java/com/nexters/ssss/conf/conf.java
@@ -3,8 +3,8 @@ package com.nexters.ssss.conf;
 
 public class conf {
 
-	public static final		String    	FILE_TEMP_PATH		= "/Users/juhyun/Documents/";
-	public static final		String		BRD_VOICE_PATH		= "/home/was/files/";
+	public static final		String    	FILE_TEMP_PATH		= "/usr/jboss/jboss-6.1.0.Final/server/ssss/deploy/ROOT.war/temp/";
+	public static final		String		BRD_VOICE_PATH		= "/usr/jboss/jboss-6.1.0.Final/server/ssss/deploy/ROOT.war/voice/";
 	
 	
 }
